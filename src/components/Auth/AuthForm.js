9ac0b99919e30abd25c030defd4d9f01a6138990
ppalladio <nine.slide@gmail.com>
2022-10-27 22:03:02 +0200

    add user signup with async

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,39 +1,79 @@
-import { useState } from 'react';
-
+import { useState, useRef } from 'react';
+//: https://firebase.google.com/docs/reference/rest/auth
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+    const [isLogin, setIsLogin] = useState(true);
+    const emailRef = useRef();
+    const passwordRed = useRef();
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const enteredEmail = emailRef.current.value;
+        const enteredPassword = passwordRed.current.value;
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+        if (isLogin) {
+        } else {
+            const sendingData = await fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB4nt-a4t-PGdiTXZ9zYXBatrQlAwMt3b0',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: enteredEmail,
+                        password: enteredPassword,
+                        returnSecureToken: true,
+                    }),
+                    header: { 'content-type': 'application/json' },
+                },
+            );
+            
+                if (sendingData.ok) {
+                } else {
+                    const errorMsg = await sendingData.json();
+                    // throw new Error(errorMsg);
+                }
+            
+
+            console.log(sendingData);
+            // const data = await sendingData.json()
+        }
+    };
+
+    return (
+        <section className={classes.auth}>
+            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor="email">Your Email</label>
+                    <input type="email" id="email" required ref={emailRef} />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="password">Your Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        required
+                        ref={passwordRed}
+                    />
+                </div>
+                <div className={classes.actions}>
+                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                    <button
+                        type="button"
+                        className={classes.toggle}
+                        onClick={switchAuthModeHandler}
+                    >
+                        {isLogin
+                            ? 'Create new account'
+                            : 'Login with existing account'}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
 };
 
 export default AuthForm;
