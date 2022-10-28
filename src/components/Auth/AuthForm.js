ed89320136806eb0872b000b6a39c4eb07c90aae
Yuxuan <nine.slide@gmail.com>
2022-10-28 14:33:06 +0200

    login in

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,93 +1,102 @@
 import { useState, useRef } from 'react';
-//: https://firebase.google.com/docs/reference/rest/auth
+
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-    const [isLogin, setIsLogin] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const emailRef = useRef();
-    const passwordRed = useRef();
-    const switchAuthModeHandler = () => {
-        setIsLogin((prevState) => !prevState);
-    };
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        const enteredEmail = emailRef.current.value;
-        const enteredPassword = passwordRed.current.value;
-        setIsLoading(true);
-        if (isLogin) {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: Add validation
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB4nt-a4t-PGdiTXZ9zYXBatrQlAwMt3b0';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB4nt-a4t-PGdiTXZ9zYXBatrQlAwMt3b0';
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
         } else {
-            const sendingData = await fetch(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB4nt-a4t-PGdiTXZ9zYXBatrQlAwMt3b0',
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        email: enteredEmail,
-                        password: enteredPassword,
-                        returnSecureToken: true,
-                    }),
-                    header: { 'content-type': 'application/json' },
-                },
-            );
-            setIsLoading(false);
-            if (sendingData.ok) {
-            } else {
-                const errorBody = await sendingData.json();
-                // console.log(errorBody);
-                const errorMsg = errorBody.error.message;
-                // console.log(errorMsg);
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
 
-                let errorMessage = 'Auth failing';
-
-                if (errorMsg && errorMsg.error && errorMsg.error.message) {
-                    errorMessage = errorMsg.error.message;
-                }
-                alert(errorMessage);
-            }
-
-            // console.log(sendingData);
-            // const data = await sendingData.json()
+            throw new Error(errorMessage);
+          });
         }
-    };
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
-    return (
-        <section className={classes.auth}>
-            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-            <form onSubmit={submitHandler}>
-                <div className={classes.control}>
-                    <label htmlFor="email">Your Email</label>
-                    <input type="email" id="email" required ref={emailRef} />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="password">Your Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        required
-                        ref={passwordRed}
-                    />
-                </div>
-                <div className={classes.actions}>
-                    {!isLoading ? (
-                        <button>{isLogin ? 'Login' : 'Create Account'}</button>
-                    ) : (
-                        <p>Loading</p>
-                    )}
-
-                    <button
-                        type="button"
-                        className={classes.toggle}
-                        onClick={switchAuthModeHandler}
-                    >
-                        {isLogin
-                            ? 'Create new account'
-                            : 'Login with existing account'}
-                    </button>
-                </div>
-            </form>
-        </section>
-    );
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default AuthForm;
